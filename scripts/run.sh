d29194fc7b4ffba984efea14f0aed57ec397f1b8
Samuel Kunst <samuel.kunst@gmail.com>
2022-04-29 11:20:16 +0200

    fix(build): prisma environment variables

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -e
 
+[ -f ./env ] && source ./env
 yarn start:prod
--- server/api/v1/words/index.get.ts
+++ server/api/v1/words/index.get.ts
@@ -1,12 +1,10 @@
 import Prisma from "@prisma/client";
 const { PrismaClient } = Prisma;
-import { CompatibilityEvent } from "h3";
 
 const prisma = new PrismaClient({
   log: ["query"],
 });
 
-export default async (event: CompatibilityEvent) => {
-  // const query = useQuery(event); // TODO: query params?
+export default async () => {
   return await prisma.word.findMany();
 };
